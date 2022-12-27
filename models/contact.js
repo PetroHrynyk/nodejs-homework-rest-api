134c76a4b5ada1cd30878f67bbecc43c2f558462
PetroHrynyk <grynyk82@gmail.com>
2022-12-27 21:17:47 +0200

    fix_Joi

--- models/contact.js
+++ models/contact.js
@@ -23,7 +23,7 @@ const contactSchema = new Schema(
 
 const joiSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string().email().required(),
   phone: Joi.string().required(),
 });
 
