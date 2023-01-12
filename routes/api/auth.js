6ddf3ba789f98ff0c679acf21adeda224757d57e
PetroHrynyk <grynyk82@gmail.com>
2023-01-12 20:27:24 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,7 +7,7 @@ const auth = require("../../middlewares/auth");
 const router = express.Router();
 
 router.post("/register", ctrl.register);
-router.post("/login", ctrl.login);
+router.post("/login", auth, ctrl.login);
 router.post("/logout", auth, ctrl.logout);
 
 module.exports = router;
